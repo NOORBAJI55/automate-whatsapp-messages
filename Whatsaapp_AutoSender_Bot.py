884952d2b0c338bced23cb706a2db1a780ac4e59
Shaik Noor Baji <137255603+NOORBAJI55@users.noreply.github.com>
2025-08-07 18:53:25 +0530

    Add files via upload

---
+++ Whatsaapp_AutoSender_Bot.py
@@ -0,0 +1,13 @@
+import pyautogui as pg
+import time
+
+print("Program will run after 5 seconds. Please focus WhatsApp Web and open the chat.")
+time.sleep(5)
+print("Sending messages...")
+
+for i in range(10):
+    pg.write("Hello, this is message number " + str(i + 1))
+    pg.press("enter")
+    time.sleep(1)
+
+print("Done!")
